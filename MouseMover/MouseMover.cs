a6534418bca021384bcdf059207ee853d65fe7fb
matmilcz <matmilcz@gmail.com>
2020-05-07 18:25:15 +0200

    Move routing to separate class

--- MouseMover/MouseMover.cs
+++ MouseMover/MouseMover.cs
@@ -10,13 +10,13 @@ namespace MouseMover
             Interval = shortInterval
         };
         private readonly ScreenAwaker screenAwaker = new ScreenAwaker();
+        private readonly MouseRouter mouseRouter = new MouseRouter();
+        private Point prevPosition = new Point();
 
         private const int shortInterval = 16; // 60 ticks per sec
-        private const int longInterval = 10000; // 10 sec
+        private const int longInterval = 10000;
 
-        private Point[] route;
-        private int routeIdx = 0;
-        private readonly int routeStep = 7;
+        private const uint routeStep = 7;
 
         private bool _enabled = false;
         public bool Enabled
@@ -43,8 +43,8 @@ namespace MouseMover
 
         private void Start()
         {
+            prevPosition = Cursor.Position;
             screenAwaker.Enabled = true;
-            routeIdx = 0;
             movementTimer.Enabled = true;
         }
 
@@ -59,70 +59,22 @@ namespace MouseMover
         {
             movementTimer.Stop();
 
-            Point previousPosition = new Point(Cursor.Position.X, Cursor.Position.Y);
+            Point currPosition = Cursor.Position;
 
-            if (routeIdx == 0)
+            if (currPosition == prevPosition)
             {
-                SetRoute();
-            }
-            
-            if (routeIdx + routeStep < route.Length)
-            {
+                Cursor.Position = mouseRouter.GetNextPoint(routeStep);
                 movementTimer.Interval = shortInterval;
-                Cursor.Position = route[routeIdx];
-                routeIdx += routeStep;
+                prevPosition = Cursor.Position;
             }
             else
             {
-                routeIdx = 0;
-            }
-
-            if (routeIdx - (2 * routeStep) > 0 && previousPosition != route[routeIdx - (2 * routeStep)]) // why does it work?
-            {
+                mouseRouter.SetRoute();
                 movementTimer.Interval = longInterval;
-                routeIdx = 0;
+                prevPosition = currPosition;
             }
 
             movementTimer.Start();
-        }
-
-        private void SetRoute()
-        {
-            Random random = new Random();
-            int destX = random.Next(1, Screen.PrimaryScreen.Bounds.Width - 1);
-            int destY = random.Next(1, Screen.PrimaryScreen.Bounds.Height - 1);
-
-            Point currentPosition = new Point(Cursor.Position.X, Cursor.Position.Y);
-            Point destinationPosition = new Point(destX, destY);
-
-            route = new Point[Math.Abs(destinationPosition.X - currentPosition.X)];
-
-            /**
-            * f(x) = mx + b
-            * route[point] = m * point + b
-            **/
-            float dx = destinationPosition.X - currentPosition.X;
-            if (dx == 0.0f)
-            {
-                dx = 1.0f; // if dx == 0, then m is whatever, I do not care
-            }
-            float m = (destinationPosition.Y - currentPosition.Y) / dx;
-            float b = currentPosition.Y - (m * currentPosition.X);
-
-            int currX = currentPosition.X;
-
-            for (int point = 0; point < route.Length; ++point)
-            {
-                route[point] = new Point(currX, (int)((m * currX) + b));
-                if (destinationPosition.X > currentPosition.X)
-                {
-                    ++currX;
-                }
-                else
-                {
-                    --currX;
-                }
-            }
         }
     }
 }
---
+++ MouseMover/MouseRouter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MouseMover
+{
+    class MouseRouter
+    {
+        private Point[] route;
+        private uint routeIdx = 0;
+
+        public Point GetCurrPoint()
+        {
+            if (routeIdx == 0)
+            {
+                return new Point(Cursor.Position.X, Cursor.Position.Y);
+            }
+            else
+            {
+                return route[routeIdx];
+            }
+        }
+
+        public Point GetNextPoint(uint routeStep)
+        {
+            if (routeIdx == 0)
+            {
+                SetRoute();
+            }
+
+            if (routeIdx + routeStep < route.Length)
+            {
+                routeIdx += routeStep;
+            }
+            else
+            {
+                SetRoute();
+                while (!(routeStep < route.Length))
+                {
+                    SetRoute();
+                }
+                routeIdx = routeStep;
+            }
+
+            return route[routeIdx];
+        }
+
+        public void SetRoute()
+        {
+            Random random = new Random();
+            int destX = random.Next(1, Screen.PrimaryScreen.Bounds.Width - 1);
+            int destY = random.Next(1, Screen.PrimaryScreen.Bounds.Height - 1);
+
+            Point currentPosition = new Point(Cursor.Position.X, Cursor.Position.Y);
+            Point destinationPosition = new Point(destX, destY);
+
+            //
+            // f(x) = mx + b
+            // route[point] = m * point + b
+            //
+            float dx = destinationPosition.X - currentPosition.X;
+            if (dx == 0.0f)
+            {
+                dx = 1.0f; // if dx == 0, then m is whatever, I do not care
+            }
+            float m = (destinationPosition.Y - currentPosition.Y) / dx;
+            float b = currentPosition.Y - (m * currentPosition.X);
+
+            route = new Point[Math.Abs(destinationPosition.X - currentPosition.X)];
+
+            int currX = currentPosition.X;
+            for (int point = 0; point < route.Length; ++point)
+            {
+                route[point] = new Point(currX, (int)((m * currX) + b));
+                if (destinationPosition.X > currentPosition.X)
+                {
+                    ++currX;
+                }
+                else
+                {
+                    --currX;
+                }
+            }
+        }
+    }
+}
